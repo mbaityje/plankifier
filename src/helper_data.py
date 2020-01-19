8f6a6bdb26f99b6c0d5866ebd408eb0aa94feed0
mbaityje <remove>
2020-01-19 20:56:25 +0100

    Added functionality to resize images without changing the proportions

---
+++ src/helper_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#
+# Helper functions for data manipulation
+#
+################################################
+from PIL import Image
+
+
+
+
+def ResizeWithProportions(im, desired_size):
+	'''
+	Take and image and resize it to a square of the desired size.
+	0) If any dimension of the image is larger than the desired size, shrink until the image can fully fit in the desired size
+	1) Add black paddings to create a square
+	'''
+
+	old_size    = im.size
+	largest_dim = max(old_size)
+	rescaled    = 0 # This flag tells us whether there was a rescaling of the image (besides the padding). We can use it as feature for training.
+
+	# 0) If any dimension of the image is larger than the desired size, shrink until the image can fully fit in the desired size
+	if max(im.size)>desired_size:
+
+		ratio = float(desired_size)/max(old_size)
+		new_size = tuple([int(x*ratio) for x in old_size])
+		im = im.resize(new_size, Image.LANCZOS)
+		rescaled = 1
+
+    # 1) Add black paddings to create a square
+	new_im = Image.new("RGB", (desired_size, desired_size), color=0)
+	new_im.paste(im, (	(desired_size-im.size[0])//2,
+						(desired_size-im.size[1])//2))
+
+	return new_im, rescaled
+
+
+
+
+if __name__=='__main__':
+	pass
--- train_smallvgg.py
+++ train_smallvgg.py
@@ -2,10 +2,10 @@
 # 
 # Train on zooplankton data with a simple multi-layer perceptron model.
 #
-# Optimizer: SGD with no weight decay, no momentum.
+# Optimizer: SGD with no weight decay.
 # 
 # Launch as:
-# 	python train_mlp.py -lr=0.05 -totEpochs=100 -width=128 -height=128 -plot
+# 	python train_smallvgg.py -lr=0.1 -totEpochs=100 -width=128 -height=128 -plot
 #
 # 
 #########################################################################
@@ -17,8 +17,7 @@ from keras.layers import Dense, Conv2D, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt, seaborn as sns
 from src.models.smallvggnet import SmallVGGNet
-from src.models.smallTom import SmallTomNet
-# import cv2 # See here for installation on Debian: https://www.pyimagesearch.com/2018/08/15/how-to-install-opencv-4-on-ubuntu/
+from src.helper_data import ResizeWithProportions
 from PIL import Image
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
@@ -30,14 +29,20 @@ parser.add_argument('-outpath', default='./out/', help="Print many messages on s
 parser.add_argument('-verbose', action='store_true', help="Print many messages on screen.")
 parser.add_argument('-plot', action='store_true', help="Plot loss and accuracy during training once the run is over.")
 parser.add_argument('-totEpochs', type=int, default=5, help="Total number of epochs for the training")
-parser.add_argument('-xtype', choices=['flat'], default='flat', help="Whether the model accepts 2D images (e.g. convnet) or flattened data (e.g. MLP)")
+parser.add_argument('-xtype', choices=['image2D'], default='image2D', help="Whether the model accepts 2D images (e.g. convnet) or flattened data (e.g. MLP)")
+parser.add_argument('-opt', choices=['sgd','adam'], default='sgd', help="Choice of the minimization algorithm (sgd,adam)")
 parser.add_argument('-bs', type=int, default=16, help="Batch size")
-parser.add_argument('-lr', type=float, default=0.01, help="Learning Rate")
-parser.add_argument('-height', type=int, default=64, help="Image height")
-parser.add_argument('-width', type=int, default=64, help="Image width")
+parser.add_argument('-lr', type=float, default=0.00005, help="Learning Rate")
+parser.add_argument('-height', type=int, default=128, help="Image height")
+parser.add_argument('-width', type=int, default=128, help="Image width")
 parser.add_argument('-depth', type=int, default=3, help="Number of channels")
 parser.add_argument('-testSplit', type=float, default=0.2, help="Fraction of examples in the validation set")
+parser.add_argument('-aug', action='store_true', help="Perform data augmentation.")
+parser.add_argument('-resize', choices=['keep_proportions','acazzo'], default='acazzo', help='The way images are resized')
 args=parser.parse_args()
+
+if args.width!=args.height:
+	raise NotImplementedError('Height and width of the image must be the same for the moment.')
 
 if args.verbose:
 	ngpu=len(keras.backend.tensorflow_backend._get_available_gpus())
@@ -47,35 +52,17 @@ np.random.seed(12345)
 # Create a unique output directory
 now = datetime.datetime.now()
 dt_string = now.strftime("%Y-%m-%d_%Hh%Mm%Ss")
-outDir = args.outpath+'/MLP/'+dt_string+'/'
+outDir = args.outpath+'/smallvgg/'+dt_string+'/'
 pathlib.Path(outDir).mkdir(parents=True, exist_ok=True)
 fsummary=open(outDir+'args.txt','w')
 print(args, file=fsummary); fsummary.flush()
 
-######################
-# DERIVED PARAMETERS #
-######################
-all_image_fnames   = [os.listdir(args.datapath+'/'+directory) for directory in os.listdir(args.datapath)]
-# num_classes        = len(all_image_fnames)
-num_images         = np.sum([len(f) for f in all_image_fnames])
-train_images       = int((1-args.testSplit)*num_images)
-test_images         = int(args.testSplit*num_images)
-n_processed_images = args.totEpochs*num_images
-tot_steps          = n_processed_images//args.bs # number of minimization steps (i.e. times the gradient is calculated)
-if args.verbose:
-	print('There are {} images in the dataset.'.format(num_images))
-	print(train_images,'go in the training dataset')
-	print(test_images,'go in the validation dataset')
-	print('num_classes:',num_classes)
-	print('tot epochs:',args.totEpochs)
-	print('n_processed_images:',n_processed_images)
-	print('tot_steps:',tot_steps)
 
 ########
 # DATA #
 ########
 data,labels = [], np.array([])
-classes = {'name': os.listdir(args.datapath)}
+classes = {'name': [ name for name in os.listdir(args.datapath) if os.path.isdir(os.path.join(args.datapath, name)) ]}
 classes['num']    = len(classes['name'])
 classes['num_ex'] =  np.zeros(classes['num'], dtype=int)
 for ic in range(classes['num']):
@@ -86,8 +73,14 @@ for ic in range(classes['num']):
 	classes['num_ex'][ic] = len(classImages) # number of examples per class
 	for imageName in classImages:
 		imagePath = classPath+imageName
+		image = Image.open(imagePath)
 
-		image = Image.open(imagePath).resize((args.width,args.height))
+		if args.resize == 'acazzo':
+			image = image.resize((args.width,args.height))
+		else:
+			# Set image's largest dimension to target size, and fill the rest with black pixels
+			image,rescaled = ResizeWithProportions(image, args.width) # width and height are assumed to be the same (assertion at the beginning)
+
 		npimage = np.array(image.copy() )
 
 		if args.xtype == 'flat':
@@ -121,34 +114,70 @@ lb = LabelBinarizer()
 trainY = lb.fit_transform(trainY)
 testY = lb.transform(testY)
 
+# construct the image generator for data augmentation
+if args.aug:
+	# aug = ImageDataGenerator(
+	# 	rotation_range=180,      width_shift_range=0.1,
+	# 	height_shift_range=0.1, shear_range=0.2, 
+	# 	zoom_range=0.2, 		horizontal_flip=True, 
+	# 	brightness_range=(0.8,1.2),
+	# 	fill_mode="constant", cval=0)
+	aug = ImageDataGenerator(
+		rotation_range=180,      width_shift_range=0.1,
+		height_shift_range=0.1, shear_range=0.2,
+		zoom_range=0.2, 		horizontal_flip=True
+		)
 
-model = Sequential()
-model.add(Dense(1024, input_shape=(len(data[0]),), activation="sigmoid"))
-model.add(Dense(512, activation="sigmoid"))
-model.add(Dense(len(lb.classes_), activation="softmax"))
+
+# initialize our VGG-like Convolutional Neural Network
+model = SmallVGGNet.build(width=args.width, height=args.height, depth=args.depth, classes=len(lb.classes_))
 
 
 # compile the model using SGD as our optimizer and categorical
 # cross-entropy loss (you'll want to use binary_crossentropy
 # for 2-class classification)
 print("[INFO] training network...")
-opt = keras.optimizers.SGD(lr=args.lr)
+if args.opt=='adam':
+	opt = keras.optimizers.Adam(learning_rate=args.lr, beta_1=0.9, beta_2=0.999, amsgrad=False)
+elif args.opt=='sgd':
+	opt = keras.optimizers.SGD(lr=args.lr, nesterov=True)
+else:
+	raise NotImplementedError('Optimizer {} is not implemented'.format(arg.opt))
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # checkpoints
 filepath = outDir+'/weights_epoch{epoch:03d}.hdf5' # make sure that the callback filepath exists, since it won't create directories
-checkpointer = keras.callbacks.ModelCheckpoint(filepath=filepath, verbose=0, save_best_only=True) # save the model at every epoch in which there is an improvement in test accuracy
+checkpointer    = keras.callbacks.ModelCheckpoint(filepath=filepath, verbose=0, save_best_only=True) # save the model at every epoch in which there is an improvement in test accuracy
+coitointerrotto = keras.callbacks.callbacks.EarlyStopping(monitor='val_loss', patience=args.totEpochs, restore_best_weights=True)
+callbacks=[checkpointer,coitointerrotto]
 
+### TRAIN ###
 
 # train the neural network
 start=time.time()
-history = model.fit(trainX, trainY, validation_data=(testX, testY), epochs=args.totEpochs, batch_size=args.bs, callbacks=[checkpointer])
+if args.aug:
+	history = model.fit_generator(
+		aug.flow(trainX, trainY, batch_size=args.bs), 
+		validation_data=(testX, testY), 
+		steps_per_epoch=len(trainX)//args.bs,	
+		epochs=args.totEpochs, 
+		callbacks=callbacks)
+else:
+	history = model.fit(
+		trainX, trainY, batch_size=args.bs, 
+		validation_data=(testX, testY), 
+		epochs=args.totEpochs, 
+		callbacks=[checkpointer, coitointerrotto])
 trainingTime=time.time()-start
 print('Training took',trainingTime/60,'minutes')
 
-# evaluate the network
+
+### evaluate the network
 print("[INFO] evaluating network...")
-predictions = model.predict(testX, batch_size=32)
+if args.aug:
+	predictions = model.predict(testX)
+else:
+	predictions = model.predict(testX, batch_size=args.bs)
 clrep=classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=classes['name'])
 print(clrep)
 
@@ -234,8 +263,9 @@ plt.savefig(outDir+'/worse-prediction.png')
 # Plot loss during training
 plt.figure(2)
 plt.title('Model loss during training')
-plt.plot(np.arange(1,args.totEpochs+1),history.history['loss'], label='train')
-plt.plot(np.arange(1,args.totEpochs+1),history.history['val_loss'], label='test')
+simulated_epochs=len(history.history['loss']) #If we did early stopping it is less than args.totEpochs
+plt.plot(np.arange(1,simulated_epochs+1),history.history['loss'], label='train')
+plt.plot(np.arange(1,simulated_epochs+1),history.history['val_loss'], label='test')
 plt.xlabel('epoch')
 plt.xlabel('loss')
 plt.legend()
@@ -245,9 +275,9 @@ plt.savefig(outDir+'/loss.png')
 plt.figure(3)
 plt.title('Model accuracy during training')
 plt.ylim((0,1))
-plt.plot(np.arange(1,args.totEpochs+1),history.history['accuracy'], label='train')
-plt.plot(np.arange(1,args.totEpochs+1),history.history['val_accuracy'], label='test')
-plt.plot(np.arange(1,args.totEpochs+1),np.ones(args.totEpochs)/classes['num'], label='random', color='black', linestyle='-.')
+plt.plot(np.arange(1,simulated_epochs+1),history.history['accuracy'], label='train')
+plt.plot(np.arange(1,simulated_epochs+1),history.history['val_accuracy'], label='test')
+plt.plot(np.arange(1,simulated_epochs+1),np.ones(simulated_epochs)/classes['num'], label='random', color='black', linestyle='-.')
 plt.xlabel('epoch')
 plt.xlabel('loss')
 plt.grid(axis='y')
@@ -272,7 +302,6 @@ plt.figure(5)
 plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.2, hspace=.7)
 ax1=plt.subplot(2, 1, 1)
 ax1.set_ylim((0,1))
-ax1.set_xlim((0,1))
 plt.title('Abstention')
 plt.ylabel('Accuracy after abstention')
 plt.xlabel('Threshold')
@@ -280,7 +309,6 @@ plt.plot(thresholds, accs, color='darkred')
 plt.grid(axis='y')
 ax2=plt.subplot(2, 1, 2)
 ax2.set_ylim((0.1,test_size*1.5))
-ax2.set_xlim((0,1))
 ax2.set_yscale('log')
 plt.ylabel('Remaining data after abstention')
 plt.xlabel('Threshold')
