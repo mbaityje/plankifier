145df967937bf0be14f238e078af1c440bed9eff
mbaityje <remove>
2020-06-03 14:12:25 +0200

    Committing changes to predict.py in order to test/debug it on the server

--- predict.py
+++ predict.py
@@ -6,21 +6,22 @@ Next upgrades:
 	- Put all data in a single pandas dataframe
 
 Launch as:
-	python predict.py -modelpath='./out/conv2/2020-02-06_17h56m55s/' -modelname='bestweights.hdf5' -testdir='../Q-AQUASCOPE/pictures/annotation_classifier/tommy_for_classifier/tommy_validation/images/' -target='daphnia'
+	python predict.py -modelpath='./out/conv2/2020-02-06_17h56m55s/' -weightsname='bestweights.hdf5' -testdir='../Q-AQUASCOPE/pictures/annotation_classifier/tommy_for_classifier/tommy_validation/images/' -target='daphnia'
 
 '''
 
 
 import os, keras, argparse, re, glob, pathlib, numpy as np
 import matplotlib.pyplot as plt, seaborn as sns
-from src import helper_models, helper_data
-from PIL import Image
+from src import helper_data as hd, helper_models as hm
+# from PIL import Image
 import tensorflow as tf
 
 
 parser = argparse.ArgumentParser(description='Load a model and use it to make predictions on images')
+parser.add_argument('-modelname', default='keras_model.h5', help='name of the model to be loaded')
 parser.add_argument('-modelpath', default='./util-files/trained-conv2/', help='directory of the model to be loaded')
-parser.add_argument('-modelname', default='bestweights.hdf5', help='name of the model to be loaded. If None, choose latest created hdf5 file in the directory')
+parser.add_argument('-weightsname', default='bestweights.hdf5', help='name of the model to be loaded. If None, choose latest created hdf5 file in the directory')
 parser.add_argument('-testdir', default='data/1_zooplankton_0p5x/validation/tommy_validation/images/dinobryon/', help='directory of the test data')
 parser.add_argument('-preddir', default='./', help='directory where you want the output to be')
 parser.add_argument('-predname', default='predict.txt', help='name of the file with the model predictions')
@@ -30,12 +31,6 @@ parser.add_argument('-PRfilter', default=None, type=float, help='Give a threshol
 parser.add_argument('-notxt', action='store_true', help='Avoid writing output to file (only have screen output)')
 args=parser.parse_args()
 
-if not args.PRfilter==None:
-	print('PRfilter=',args.PRfilter)
-	if (args.PRfilter<1):
-		print('args.PRfilter should be >=1, so we cannot accept ',args.PRfilter)
-		raise ValueError
-
 if not args.verbose:
 	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 	tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
@@ -44,82 +39,21 @@ if not os.path.isdir(args.testdir):
 	print('You told me to read the images from a directory that does not exist:\n{}'.format(args.testdir))
 	raise IOError
 
-
-def CheckArgs(modelpath):
-	'''
-	Looks with what arguments the model was trained, and makes a series of consistency checks.
-	'''
-	argsname=modelpath+'args.txt'
-
-	# Read Arguments
-	with open(argsname,'r') as fargs:
-		args=fargs.read()
-		print('---------- Arguments for generation of the model ----------')
-		print(args)
-		print('-----------------------------------------------------------')
-		layers=[None,None]
-		for s in re.split('[\,,\),\(]',args):
-		    if 'height' in s:
-		        height=np.int64(re.search('(\d+)',s).group(1))
-		    if 'width' in s:
-		        width=np.int64(re.search('(\d+)',s).group(1))
-		    if 'depth' in s:
-		        depth=np.int64(re.search('(\d+)',s).group(1))
-		    if 'model' in s:
-		        modelname=re.search('=\'(.+)\'$',s).group(1)
-		    if 'resize' in s:
-		        resize=re.search('=\'(.+)\'$',s).group(1)
-		    if 'layers' in s: #first layer
-		        layers[0]=np.int64(re.search('=\[(.+)$',s).group(1))
-		    if re.match('^ \d+',s): #second layer
-		        layers[1]=np.int64(re.match('^ (\d+)',s).group(1))
-		    if 'datapath' in s:
-		        datapath=re.search('=\'(.+)\'$',s).group(1)
-		    if 'outpath' in s:
-		        outpath=re.search('=\'(.+)\'$',s).group(1)
-
-	print('height: ', height)
-	print('width: ', width)
-	print('depth: ', depth)
-	print('model name: ', modelname)
-	print('resize: ', resize)
-	print('layers: ', layers)
-	print('datapath: ', datapath)
-	print('outpath: ', outpath)
-	print('-----------------------------------------------------------')
-
-	# Extract classes from npy file in output directory
-	classes_dict=np.load(modelpath+'classes.npy',allow_pickle=True).item()
-
-	def OutputClassPaths():
-		print('\nclasses_dict full filename: ',modelpath+'classes.npy')
-		print('classes_dict[\'name\']:',classes_dict['name'])
-		print('\nclasses are the subdirectories in the following folder: ', datapath)
-		print('classes:',classes)
-		print('')
-
-	try:
-		# Extract classes from data directory (if datapath is accessible)
-		classes = [ name for name in os.listdir(datapath) if os.path.isdir(os.path.join(datapath, name)) ]
-	except FileNotFoundError:
-		pass
-	else:
-		# Compare the two
-		if len(classes_dict['name']) != len(classes):
-			OutputClassPaths()
-			raise IndexError('Number of classes in data directory is not the same as in classes.npy')
-		if not np.all(set(classes_dict['name'])==set(classes)):
-			OutputClassPaths()
-			raise ValueError('Some of the classes in data directory are not the same as in classes.npy')
+# Read parameters of the model that is loaded
+params, classes = hd.ReadArgsTxt(args.modelpath)
 
 
-	return height, width, depth, modelname, resize, layers, datapath, outpath, classes_dict
 
-height, width, depth, modelname, resize, layers, datapath, outpath, classes_dict = CheckArgs(args.modelpath)
+sys.exit('fine prova')
+
 
 #If PR filter is unset, just set it to the maximum value that it can assume (i.e. the number of classes)
-if args.PRfilter==None:
-	args.PRfilter=classes_dict['num']
+if args.PRfilter is None: 	args.PRfilter=len(classes)
+else:
+	if (args.PRfilter<1):
+		print('args.PRfilter should be >=1, so we cannot accept ',args.PRfilter)
+		raise ValueError
+
 
 # Read names of images that we want to predict on.
 im_names=np.array(glob.glob(args.testdir+'/*.jpeg'),dtype=object)
@@ -134,7 +68,10 @@ if len(im_names)<1:
 else:
 	print('There are {} images in {}'.format(len(im_names), args.testdir))
 
-if args.modelname == None:
+
+
+# If no model name is given, select the newest one contained in the directory
+if args.weightsname is None:
 	from stat import S_ISREG, ST_CTIME, ST_MODE
 	# Choose model. We load the latest created .hdf5 file, since later is better
 	entries = [modelpath+'/'+entry for entry in os.listdir(modelpath) if '.hdf5' in entry]
@@ -143,52 +80,36 @@ if args.modelname == None:
 	modelfile=sorted(entries)[-1][1]
 	print('Loading last generated model'.format(modelfile))
 else:
-	modelfile = args.modelpath+args.modelname
-	print('Loading {}'.format(modelfile))
+	modelfile = args.modelpath+args.weightsname
+	print('modelfile: {}'.format(modelfile))
 
-# Initialize and load model
-model=keras.models.load_model(modelfile)
-
-def load_images(im_names, width, height, depth, modelname, resize):
-    ''' 
-    Function that loads a list of images given in im_names, and returns 
-    them in a numpy format that can be used by the classifier
-    '''
-    npimages=np.ndarray((len(im_names),width,height,depth)) if modelname != 'mlp' else np.ndarray((len(im_names),width*height*depth))
-
-    for i,im_name in enumerate(im_names):
-        image = Image.open(im_name)
-        if resize == 'acazzo':
-            image = image.resize((width,height))
-        else:
-            # Set image's largest dimension to target size, and fill the rest with black pixels
-            if width!=height:
-            	print('load_images(): width={}, height={}'.format(width,height))
-            	raise NotImplementedError('We are only treating square, not rectangular images')
-            image,rescaled = helper_data.ResizeWithProportions(image, width) # width and height are assumed to be the same
-            npimage=np.array(image.copy())
-        if model == 'mlp':
-            npimage = npimage.flatten()
-        npimages[i]=npimage
-        if len(im_names)==1:
-            image.show()
-        image.close()    
-    return npimages/255.0
 
 
 # Load images
-npimages=load_images(im_names, width, height, depth, modelname, resize)
+npimages=hd.LoadImageList(im_names, L, show=False)
+
+# Initialize and load model
+# model=keras.models.load_model(modelfile)
+params = {
+		'model': 'conv2', 
+		'train': False, 
+		'load': modelfile,
+		'numclasses': len(classes),
+		'lr': 0, # This number should not be relevant, since we don't train
+		}
+history, model = hm.PlainModel(npimages, None, None, None, params)
+
 
 
 # Print prediction
 probs=model.predict(npimages)
-predictions=probs.argmax(axis=1)  # The class that the classifier wuold bet on
+predictions=probs.argmax(axis=1)  # The class that the classifier would bet on
 confidences=probs.max(axis=1)     # equivalent to: [probs[i][predictions[i]] for i in range(len(probs))] 
-predictions_names=np.array([classes_dict['name'][predictions[i]] for i in range(len(npimages))],dtype=object)
+predictions_names=np.array([classes[predictions[i]] for i in range(len(npimages))],dtype=object)
 
 predictions2=probs.argsort(axis=1)[:,-2] # The second most likely class
 confidences2=np.array([probs[i][predictions2[i]] for i in range(len(probs))], dtype=float) 
-predictions2_names=np.array([classes_dict['name'][predictions2[i]] for i in range(len(npimages))],dtype=object)
+predictions2_names=np.array([classes[predictions2[i]] for i in range(len(npimages))],dtype=object)
 
 
 # Participation ratio
@@ -223,7 +144,7 @@ print(header)
 for i in range(len(npimages)):
 	
 	if pr[i]<=args.PRfilter:
-		print('{}\t{:20s}\t{:.3f}\t{:20s}\t{:.3f}\t{:.3f}'.format(im_names[i], classes_dict['name'][predictions[i]], confidences[i], classes_dict['name'][predictions2[i]], confidences2[i], pr[i] ) )
+		print('{}\t{:20s}\t{:.3f}\t{:20s}\t{:.3f}\t{:.3f}'.format(im_names[i], classes[predictions[i]], confidences[i], classes[predictions2[i]], confidences2[i], pr[i] ) )
 
 
 
