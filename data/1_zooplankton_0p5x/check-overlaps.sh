611dfe738c84941a80cf6c8b47daac0c51d4075e
mbaityje <remove>
2020-08-04 17:19:38 +0200

    Minor change

--- data/1_zooplankton_0p5x/check-overlaps.sh
+++ data/1_zooplankton_0p5x/check-overlaps.sh
@@ -3,6 +3,7 @@
 echo "For each dataset, we search for duplicates in the other datasets"
 for dataset in $(ls training/* -d)
 do
+    echo ""
     echo dataset: $dataset
     echo "The following images are also found in other datasets"
     for image in $(ls $dataset/*/training_data/*.jpeg)
@@ -12,9 +13,14 @@ do
 	    other_image=$(echo $image | sed "s-$dataset-$other_dataset-")
 
 	    # If the ls finds something, it is a duplicate
-	    ls $other_image 2>/dev/null
+	    if [ $(ls $other_image 2>/dev/null) ]
+	    then
+		echo $image
+		echo "-- $other_image"
+	    fi
+	    
 	done
     done
 		 
 done
-		     
+
