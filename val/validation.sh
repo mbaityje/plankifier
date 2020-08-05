5c8cc15b0050c40920704dd7609fd6a090983b20
mbaityje <remove>
2020-08-05 11:15:08 +0200

    Changes to validation

--- val/validation.sh
+++ val/validation.sh
@@ -8,10 +8,17 @@
    
 
 
-FILE_RESULTS=results.txt
 HERE=$PWD
 PROGSDIR=$PWD/..
-
+OUTDIR=$HERE/pred_temp/
+FILE_RESULTS=$OUTDIR/results.txt
+models="out/trained-models/conv2_image_adam_aug_b8_lr1e-3_L128_t500/keras_model.h5 \
+out/trained-models/conv2_image_sgd_aug_b32_lr5e-5_L128_t1000/keras_model.h5 \
+out/trained-models/smallvgg_image_adam_aug_b32_lr1e-3_L128_t5000/keras_model.h5 \
+out/trained-models/smallvgg_image_adam_aug_b32_lr5e-5_L128_t5000/keras_model.h5 \
+out/trained-models/smallvgg_image_sgd_aug_b8_lr5e-6_L192_t5000/keras_model.h5 \
+out/trained-models/smallvgg_image_sgd_aug_b32_lr5e-5_L128_t5000/keras_model.h5
+"
 
 # Loop over the directories contained in validation.txt
 echo "class good tot ratio" > $FILE_RESULTS
@@ -19,20 +26,21 @@ while IFS= read -r line; do
 
     # Each line in validation.txt contains a class name, and a path to images of that class
     read class file <<<$line
+    echo $class
+
     cd $PROGSDIR
 
-    preds=$(python predict.py -testdir=$file | grep .jpeg | awk '{print $2}')
-    temp=$(echo "$preds" | awk -vclass=$class 'BEGIN{good=0; tot=0}{tot+=1.0; if(class==$1) {good+=1;}}END{print good,tot,good/tot}')
+    python predict.py -testdir=$file -outpath=$OUTDIR -predname=$class -modelfullname $models -em='leader' -absthres=0.8 2>/dev/null
 
-#    good=$(echo $temp | cut -d' ' -f1)
-#    tot=$(echo $temp | cut -d' ' -f2)
-#    ratio=$(echo $temp |cut -d' ' -f3)
-    echo $class $temp
+    predfile=$(echo $OUTDIR/$class.txt)
+    temp=$(awk -vclass=$class 'BEGIN{good=0; tot=0}{tot+=1.0; if(class==$2){good+=1;}}END{print good,tot,good/tot}' $predfile)
 
-    cd $HERE
-done < validation.txt >> $FILE_RESULTS
+    echo $class $temp >> $FILE_RESULTS
+    cd $HERE	
+done < validation.txt 
 
 # Make a classification report based on the outcome of the previous loop (results.txt)
-python class_report.py
+cd $OUTDIR
+python $HERE/class_report.py >> $FILE_RESULTS
 
 
