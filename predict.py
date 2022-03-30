a3f849d2fb9dba2d329aa5378c29cb59fdbbbb8b
Sreenath P Kyathanahally <pruthvi.iyengar@gmail.com>
2022-03-30 13:18:43 +0200

    Update predict.py
    Predict.py was stopping the predictions when one of the input images  had desired ratio less than 4 pixels. Now, the minimum size was reduced to 1 pixel and then if it is less than that, the image is resized without keeping the aspect ratio to desired size. This specific filename will be then nted out"

--- predict.py
+++ predict.py
@@ -31,8 +31,8 @@ import tensorflow as tf
 from collections import Counter
 from pathlib import Path
 
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 __UNCLASSIFIED__ = 'Unclassified'
 
